e0561e1ce478bdd255f2e355392e549c65112c60
0385246651 <haibaba2k@gmail.com>
2026-01-02 11:53:40 +0700

    feat: thêm responsive cho TV 4K 50+ inch

--- src/components/movie/MovieGrid.tsx
+++ src/components/movie/MovieGrid.tsx
@@ -25,7 +25,7 @@ export function MovieGrid({ movies, loading }: MovieGridProps) {
   }
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 3xl:grid-cols-8 4xl:grid-cols-10 gap-4 md:gap-6 3xl:gap-8">
       {movies.map((movie, index) => (
         <MovieCard
           key={movie._id || movie.slug}
